c203078e7c73c825bd8e32b17bf248bc802e53e5
edgar <edgar.hovhannisyan08@gmail.com>
2020-02-04 19:05:09 +0400

    the categs and news sections are already created

--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -1,7 +1,29 @@
 // Hero slider handler
-$('#owl-demo').owlCarousel({
+$('#hr-sld').owlCarousel({
     loop: true,
     nav: true,
+    dots: false,
     navText: [$('#arrow-prev'), $('#arrow-next')],
     items: 1 
+})
+
+// Partners slider handler
+$('#prt-sld').owlCarousel({
+    loop: true,
+    margin: 50,
+    nav: true,
+    responsive: {
+        0:{
+            items:1
+        },
+        480: {
+            items:2
+        },
+        768:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
 })
