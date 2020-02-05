7d0b493edd38cea78fdff3a09a80b3cadb231b5f
edgar <edgar.hovhannisyan08@gmail.com>
2020-02-05 13:44:43 +0400

    Partners section is created

--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -1,29 +1,39 @@
-// Hero slider handler
-$('#hr-sld').owlCarousel({
-    loop: true,
-    nav: true,
-    dots: false,
-    navText: [$('#arrow-prev'), $('#arrow-next')],
-    items: 1 
+$(document).ready(function() {
+    // Hero slider handler
+    $('#hr-sld').owlCarousel({
+        loop: true,
+        dots: false,
+        nav: true,
+        navText: [$('#hr-arrow-prev'), $('#hr-arrow-next')],
+        items: 1,
+        autoplay: true,
+        smartSpeed: 1500
+    })
+
+    // Partners slider handler
+    $('#prt-sld').owlCarousel({
+        loop: true,
+        items:4,
+        margin: 50,
+        dots: false,
+        nav: true,
+        navText: [$('#prt-arrow-prev'), $('#prt-arrow-next')],
+        autoWidth: true,
+        lazyLoad: true,
+        autoplay: true,
+        smartSpeed: 1500,
+        responsive: {
+            0:{
+                items:1
+            },
+            480: {
+                items:2
+            },
+            1024:{
+                items:3
+            }
+        }
+    })
 })
 
-// Partners slider handler
-$('#prt-sld').owlCarousel({
-    loop: true,
-    margin: 50,
-    nav: true,
-    responsive: {
-        0:{
-            items:1
-        },
-        480: {
-            items:2
-        },
-        768:{
-            items:3
-        },
-        1000:{
-            items:4
-        }
-    }
-})
+
