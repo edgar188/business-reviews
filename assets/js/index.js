6938e59c498f6dfa624c0aa9142edf431ea97b5c
edgar <edgar.hovhannisyan08@gmail.com>
2020-02-08 16:48:31 +0400

    Map bug fix, and created footer section

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,7 +3,13 @@ function initMap() {
     var uluru = {lat: -25.344, lng: 131.036};
     // The map, centered at Uluru
     var map = new google.maps.Map(
-        document.getElementById('map'), {zoom: 4, center: uluru});
+        document.getElementById('map'), {
+            zoom: 4, 
+            center: uluru,
+            mapTypeId: 'hybrid',
+            disableDefaultUI: true
+    });
     // The marker, positioned at Uluru
-    var marker = new google.maps.Marker({position: uluru, map: map});
+    // var marker = new google.maps.Marker({position: uluru, map: map});
+    setMapOnAll(null);
 }
