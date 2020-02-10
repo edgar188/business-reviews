ba86cbef359e011c3ffe4c09b265c74763ff5365
edgar <edgar.hovhannisyan08@gmail.com>
2020-02-10 18:15:04 +0400

    Created responsive header

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,25 @@
+var bool = false;
+var navList = document.getElementById("nav-list");
+var btnNav = document.getElementById("btn-nav");
+var btnClose = document.getElementById("btn-close");
+var body = document.body;
+
+function onNavBtn() {
+    bool = !bool;
+
+    if(bool) {
+        navList.className += "show";
+        btnNav.className = "btn-clear nav hidden";
+        btnClose.className = "btn-clear close visible";
+        body.style.overflowY = "hidden";
+    } else {
+        navList.className = "";
+        btnNav.className = "btn-clear nav visible";
+        btnClose.className = "btn-clear close hidden";
+        body.style.overflowY = "visible";
+    }
+}
+
 function initMap() {
     // The location of Uluru
     var uluru = {lat: -25.344, lng: 131.036};
